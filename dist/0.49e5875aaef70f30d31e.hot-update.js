2c3928c2f36431c190489ad77876905c645cff9d
Josh Cox <cox.josh.d@gmail.com>
2018-03-24 11:33:22 -0500

    Refactor: Update structure to match the webpack configuration of the taproom

---
+++ dist/0.49e5875aaef70f30d31e.hot-update.js
@@ -0,0 +1,9 @@
+webpackHotUpdate(0,[
+/* 0 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: SyntaxError: Unexpected token (106:19)\n\n\u001b[0m \u001b[90m 104 | \u001b[39m        exact\n \u001b[90m 105 | \u001b[39m        path \u001b[33m=\u001b[39m \u001b[32m\"/\"\u001b[39m\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 106 | \u001b[39m        render \u001b[33m=\u001b[39m {()\n \u001b[90m     | \u001b[39m                   \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 107 | \u001b[39m    \u001b[33m=>\u001b[39m \u001b[33m<\u001b[39m\n \u001b[90m 108 | \u001b[39m        h1 \u001b[33m>\u001b[39m \u001b[33mHome\u001b[39m \u001b[33m<\u001b[39m \u001b[35m/h1>} /\u001b[39m \u001b[33m>\u001b[39m\n \u001b[90m 109 | \u001b[39m        \u001b[33m<\u001b[39m \u001b[33mRoute\u001b[39m\u001b[0m\n");
+
+/***/ })
+])
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIwLjQ5ZTU4NzVhYWVmNzBmMzBkMzFlLmhvdC11cGRhdGUuanMiLCJzb3VyY2VSb290IjoiIn0=
---
+++ src/main/index.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+import {
+    AppBar,
+    Drawer,
+    IconButton,
+    Reboot,
+    Toolbar,
+    Typography
+} from 'material-ui';
+import List, {ListItem, ListItemIcon, ListItemText} from 'material-ui/List';
+
+import {AccountCircle, Menu, Home} from 'material-ui-icons';
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.toggleMenuState = this.toggleMenuState.bind(this);
+        this.state = {
+            menuOpen: false
+        }
+    }
+
+    toggleMenuState() {
+        this.setState({menuOpen: !this.state.menuOpen});
+    }
+
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    {/* Normalize the CSS */}
+                    <Reboot/>
+
+                    {/* Header */}
+                    <AppBar position="static">
+                        <Toolbar disableGutters={true}>
+                            <IconButton color="inherit" onClick={this.toggleMenuState}>
+                                <Menu/>
+                            </IconButton>
+                            <Typography variant="title" color="inherit">
+                                Admin
+                            </Typography>
+                        </Toolbar>
+                    </AppBar>
+
+                    {/* Menu */}
+                    <Drawer
+                        anchor="left"
+                        open={this.state.menuOpen}
+                        onClose={this.toggleMenuState}
+                        variant="temporary"
+                    >
+                        <List>
+                            <Link to="/">
+                                <ListItem button onClick={this.toggleMenuState}>
+                                    <ListItemIcon>
+                                        <Home/>
+                                    </ListItemIcon>
+                                    <ListItemText primary="Home"/>
+                                </ListItem>
+                            </Link>
+                            <Link to="/users">
+                                <ListItem button onClick={this.toggleMenuState}>
+                                    <ListItemIcon>
+                                        <AccountCircle/>
+                                    </ListItemIcon>
+                                    <ListItemText primary="Users"/>
+                                </ListItem>
+                            </Link>
+                        </List>
+                    </Drawer>
+
+                    {/* Content */}
+                    <Switch>
+                        <Route exact path="/" render={() => <h1>Home</h1>}/>
+                        <Route path="/users" render={() => <h1>Users</h1>}/>
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        );
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
